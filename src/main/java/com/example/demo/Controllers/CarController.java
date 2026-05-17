1d00f88af78817fb5fbc53633825b31e50a30623
Yuk1210 <daffyangel1210@gmail.com>
2026-05-18 00:08:01 +0200

    Okay..... Har tilføjet title + lang til html Har tilføjet farve = status på index Har gjort så vælg bil på opret skader + aftaler virker
    Alle klasser der er blevet ændret i:
    aftaler.html
    CarController
    DamageReportController
    HomeController
    index.html
    JbdcCarRepository
    opret-aftale.html
    opret-skade.html
    RentalAgreement
    sidebar.html
    style.css

--- src/main/java/com/example/demo/Controllers/CarController.java
+++ src/main/java/com/example/demo/Controllers/CarController.java
@@ -1,13 +1,23 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Services.CarService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CarController {
 
+    private final CarService service;
+
+    public CarController(CarService service) {
+        this.service = service;
+    }
+
     @GetMapping("/biler")
-    public String showCars() {
+    public String showCars(Model model) {
+
+  //      model.addAttribute("cars", service.getAll());
         return "biler";
     }
 
--- src/main/java/com/example/demo/Controllers/HomeController.java
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -1,12 +1,26 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Services.CarService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    private final CarService carService;
+
+    public HomeController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("availableCount", carService.countAvailableCars());
+        model.addAttribute("rentedCount", carService.countRentedCars());
+        model.addAttribute("damagedCount", carService.countDamagedCars());
+
+        model.addAttribute("cars", carService.getAll());
         return "index";
     }
 }
