30c54926d000e578f4cec9f1f7a6c807f30fb65b
ImraneRabhi801 <imranerabhi10@gmail.com>
2026-05-21 14:58:27 +0200

    jo

--- src/main/java/com/example/demo/Controllers/UserController.java
+++ src/main/java/com/example/demo/Controllers/UserController.java
@@ -54,4 +54,11 @@ public class UserController {
 
         return "fragments/Login";
     }
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+
+        session.invalidate();
+
+        return "redirect:/login";
+    }
 }
